ba60797d1d1b68e24ed977a66532e502463cc050
James Dozier <almostsurelyape@gmail.com>
2015-02-23 10:16:47 -0600

    Further connection tests added.

--- BlabberBotTests.py
+++ BlabberBotTests.py
@@ -16,13 +16,27 @@ class BlabberBotTests(unittest.TestCase):
         :return:
         """
 
-        #Test that our config file exists.
+        # Test that our config file exists.
         self.assertTrue(os.path.isfile(r'./blabberbot.cfg'))
 
         config = configparser.ConfigParser()
         config.read(r'./blabberbot.cfg')
 
-        #Test that necessary headers exist.
+        # Test that necessary headers exist.
         self.assertTrue('Twitter' in config.keys())
         self.assertTrue('Neo4j' in config.keys())
         self.assertTrue('Dictionary' in config.keys())
+
+        # Test that necessary values under headers exist.
+        # Twitter
+        self.assertTrue('token' in config['Twitter'])
+        self.assertTrue('token_secret' in config['Twitter'])
+        self.assertTrue('consumer' in config['Twitter'])
+        self.assertTrue('consumer_secret' in config['Twitter'])
+
+        # Neo4j
+        self.assertTrue('user' in config['Neo4j'])
+        self.assertTrue('password' in config['Neo4j'])
+
+        # Dictionary
+        self.assertTrue('key' in config['Dictionary'])
